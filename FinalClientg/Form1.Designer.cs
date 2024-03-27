406e1561f690c849d5c53439b5d36f17e00398bb
yeju <dpwn9811@naver.com>
2024-03-27 10:50:16 +0900

    통신완료

--- FinalClientg/Form1.Designer.cs
+++ FinalClientg/Form1.Designer.cs
@@ -28,93 +28,115 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.text_IP = new System.Windows.Forms.TextBox();
-			this.label1 = new System.Windows.Forms.Label();
-			this.label2 = new System.Windows.Forms.Label();
-			this.text_PORT = new System.Windows.Forms.TextBox();
-			this.BTN_CONNECT = new System.Windows.Forms.Button();
-			this.groupBox1 = new System.Windows.Forms.GroupBox();
-			this.BTN_DISCONNECT = new System.Windows.Forms.Button();
-			this.groupBox1.SuspendLayout();
-			this.SuspendLayout();
-			// 
-			// text_IP
-			// 
-			this.text_IP.Location = new System.Drawing.Point(167, 98);
-			this.text_IP.Name = "text_IP";
-			this.text_IP.Size = new System.Drawing.Size(264, 35);
-			this.text_IP.TabIndex = 0;
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(76, 101);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(85, 24);
-			this.label1.TabIndex = 1;
-			this.label1.Text = "IP 주소";
-			// 
-			// label2
-			// 
-			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(51, 142);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(110, 24);
-			this.label2.TabIndex = 3;
-			this.label2.Text = "Port 번호";
-			// 
-			// text_PORT
-			// 
-			this.text_PORT.Location = new System.Drawing.Point(167, 139);
-			this.text_PORT.Name = "text_PORT";
-			this.text_PORT.Size = new System.Drawing.Size(264, 35);
-			this.text_PORT.TabIndex = 2;
-			// 
-			// BTN_CONNECT
-			// 
-			this.BTN_CONNECT.Location = new System.Drawing.Point(167, 181);
-			this.BTN_CONNECT.Name = "BTN_CONNECT";
-			this.BTN_CONNECT.Size = new System.Drawing.Size(264, 42);
-			this.BTN_CONNECT.TabIndex = 4;
-			this.BTN_CONNECT.Text = "접속";
-			this.BTN_CONNECT.UseVisualStyleBackColor = true;
-			// 
-			// groupBox1
-			// 
-			this.groupBox1.Controls.Add(this.BTN_DISCONNECT);
-			this.groupBox1.Location = new System.Drawing.Point(28, 43);
-			this.groupBox1.Name = "groupBox1";
-			this.groupBox1.Size = new System.Drawing.Size(465, 257);
-			this.groupBox1.TabIndex = 5;
-			this.groupBox1.TabStop = false;
-			this.groupBox1.Text = "CONNECT";
-			// 
-			// BTN_DISCONNECT
-			// 
-			this.BTN_DISCONNECT.Location = new System.Drawing.Point(139, 186);
-			this.BTN_DISCONNECT.Name = "BTN_DISCONNECT";
-			this.BTN_DISCONNECT.Size = new System.Drawing.Size(264, 42);
-			this.BTN_DISCONNECT.TabIndex = 6;
-			this.BTN_DISCONNECT.Text = "접속 해제";
-			this.BTN_DISCONNECT.UseVisualStyleBackColor = true;
-			// 
-			// Form1
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(524, 357);
-			this.Controls.Add(this.BTN_CONNECT);
-			this.Controls.Add(this.label2);
-			this.Controls.Add(this.text_PORT);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.text_IP);
-			this.Controls.Add(this.groupBox1);
-			this.Name = "Form1";
-			this.Text = "Form1";
-			this.Load += new System.EventHandler(this.Form1_Load);
-			this.groupBox1.ResumeLayout(false);
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.text_IP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.text_PORT = new System.Windows.Forms.TextBox();
+            this.BTN_CONNECT = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.BTN_DISCONNECT = new System.Windows.Forms.Button();
+            this.ListBox_MSG = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // text_IP
+            // 
+            this.text_IP.Location = new System.Drawing.Point(90, 49);
+            this.text_IP.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.text_IP.Name = "text_IP";
+            this.text_IP.Size = new System.Drawing.Size(144, 21);
+            this.text_IP.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 50);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP 주소";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 71);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Port 번호";
+            // 
+            // text_PORT
+            // 
+            this.text_PORT.Location = new System.Drawing.Point(75, 55);
+            this.text_PORT.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.text_PORT.Name = "text_PORT";
+            this.text_PORT.Size = new System.Drawing.Size(144, 21);
+            this.text_PORT.TabIndex = 2;
+            // 
+            // BTN_CONNECT
+            // 
+            this.BTN_CONNECT.Location = new System.Drawing.Point(75, 80);
+            this.BTN_CONNECT.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.BTN_CONNECT.Name = "BTN_CONNECT";
+            this.BTN_CONNECT.Size = new System.Drawing.Size(142, 21);
+            this.BTN_CONNECT.TabIndex = 4;
+            this.BTN_CONNECT.Text = "접속";
+            this.BTN_CONNECT.UseVisualStyleBackColor = true;
+            this.BTN_CONNECT.Click += new System.EventHandler(this.BTN_CONNECT_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.BTN_CONNECT);
+            this.groupBox1.Controls.Add(this.text_PORT);
+            this.groupBox1.Controls.Add(this.BTN_DISCONNECT);
+            this.groupBox1.Location = new System.Drawing.Point(15, 22);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(250, 145);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "CONNECT";
+            // 
+            // BTN_DISCONNECT
+            // 
+            this.BTN_DISCONNECT.Location = new System.Drawing.Point(75, 105);
+            this.BTN_DISCONNECT.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.BTN_DISCONNECT.Name = "BTN_DISCONNECT";
+            this.BTN_DISCONNECT.Size = new System.Drawing.Size(142, 21);
+            this.BTN_DISCONNECT.TabIndex = 6;
+            this.BTN_DISCONNECT.Text = "접속 해제";
+            this.BTN_DISCONNECT.UseVisualStyleBackColor = true;
+            // 
+            // ListBox_MSG
+            // 
+            this.ListBox_MSG.FormattingEnabled = true;
+            this.ListBox_MSG.ItemHeight = 12;
+            this.ListBox_MSG.Location = new System.Drawing.Point(15, 182);
+            this.ListBox_MSG.Name = "ListBox_MSG";
+            this.ListBox_MSG.Size = new System.Drawing.Size(250, 148);
+            this.ListBox_MSG.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 346);
+            this.Controls.Add(this.ListBox_MSG);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.text_IP);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
@@ -127,6 +149,7 @@
 		private System.Windows.Forms.Button BTN_CONNECT;
 		private System.Windows.Forms.GroupBox groupBox1;
 		private System.Windows.Forms.Button BTN_DISCONNECT;
-	}
+        private System.Windows.Forms.ListBox ListBox_MSG;
+    }
 }
 
--- FinalClientg/Form1.cs
+++ FinalClientg/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,14 +14,79 @@ namespace FinalClientg
 {
 	public partial class Form1 : Form
 	{
-		public Form1()
+        private const int bufferSize = 1024; // 버퍼 크기 설정
+        private TcpClient client;
+        private NetworkStream stream;
+        private byte[] buffer;
+
+        public Form1()
 		{
-			InitializeComponent();
-		}
+            InitializeComponent();
+            buffer = new byte[bufferSize];
+        }
 
 		private void Form1_Load(object sender, EventArgs e)
 		{
 
 		}
-	}
+
+        private void BTN_CONNECT_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // 이미 연결되어 있는 경우 연결 중복 방지
+                if (client != null && client.Connected)
+                {
+                    MessageBox.Show("이미 서버에 연결되어 있습니다.");
+                    return;
+                }
+
+                // TcpClient 객체 생성 및 서버에 연결
+                client = new TcpClient();
+                client.Connect(text_IP.Text, int.Parse(text_PORT.Text));
+                //StatusLabel.Text = "Connected to server.";
+
+                // 네트워크 스트림 얻기
+                stream = client.GetStream();
+
+                // 비동기로 서버로부터 데이터 수신 대기
+                stream.BeginRead(buffer, 0, buffer.Length, new AsyncCallback(OnDataReceived), null);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("연결 중 오류 발생: " + ex.Message);
+            }
+        }
+
+        // 비동기 수신 완료 콜백 함수
+        private void OnDataReceived(IAsyncResult ar)
+        {
+            try
+            {
+                int bytesRead = stream.EndRead(ar);
+                if (bytesRead > 0)
+                {
+                    string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                    // 받은 메시지를 UI에 표시하기 위해 Invoke 사용
+                    Invoke(new Action(() => ListBox_MSG.Items.Add(dataReceived)));
+
+                    // 다시 비동기로 데이터 수신 대기
+                    stream.BeginRead(buffer, 0, buffer.Length, new AsyncCallback(OnDataReceived), null);
+                }
+                else
+                {
+                    // 연결 종료
+                    stream.Close();
+                    client.Close();
+                    //StatusLabel.Text = "Disconnected from server.";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("데이터 수신 중 오류 발생: " + ex.Message);
+            }
+        }
+    
+
+    }
 }
